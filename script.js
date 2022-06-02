274d0144c4e00771ee8a7b4d7e3304d6b18decf4
Taha Attibbi <tahaatb@gmail.com>
2022-06-02 22:09:01 +0100

    removed transparency from die and fixed bank system and changed section color when 100 score is reached

--- script.js
+++ script.js
@@ -40,6 +40,7 @@ roll.addEventListener("click", function () {
       currentPlayer++;
       img.innerHTML = `<img src="images/${rand}-die.png" alt="" /></div>`;
       //   num1.textContent = `${currentTotal1}`;
+
       currentTotal1 = 0;
       currNum1.textContent = `${currentTotal1}`;
       leftTurn();
@@ -52,12 +53,37 @@ roll.addEventListener("click", function () {
       currentPlayer++;
       img.innerHTML = `<img src="images/${rand}-die.png" alt="" /></div>`;
       //   num2.textContent = `${currentTotal2}`;
+
       currentTotal2 = 0;
       currNum2.textContent = `${currentTotal2}`;
       rightTurn();
     } else {
       currentUpdate2();
       leftTurn();
+    }
+  }
+});
+
+hold.addEventListener("click", function () {
+  if (currentPlayer % 2 != 0) {
+    bank1 += currentTotal1;
+    num1.textContent = `${bank1}`;
+    currentTotal1 = 0;
+    currNum1.textContent = `${currentTotal1}`;
+    currentPlayer++;
+    if (bank1 >= 100) {
+      leftHighlight.style.backgroundColor = "#302c2c";
+      document.querySelector(".player-1").style.color = "#FFFFFF";
+    }
+  } else {
+    bank2 += currentTotal2;
+    num2.textContent = `${bank2}`;
+    currentTotal2 = 0;
+    currNum2.textContent = `${currentTotal2}`;
+    currentPlayer++;
+    if (bank2 >= 100) {
+      leftHighlight.style.backgroundColor = "#302c2c";
+      document.querySelector(".player-2").style.color = "#FFFFFF";
     }
   }
 });
