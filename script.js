5d4cca3a5b7c71e3a3d02dbce8bdb9138c72aeab
Taha Attibbi <tahaatb@gmail.com>
2022-06-02 15:48:23 +0100

    Refactored some code and almost completed currentUpdate system, also changed the color of the sections when player turn changes

--- script.js
+++ script.js
@@ -5,13 +5,43 @@ const hold = document.querySelector(".hold");
 const img = document.querySelector(".die");
 const currNum1 = document.querySelector(".current-number-1");
 const currNum2 = document.querySelector(".current-number-2");
+const leftHighlight = document.querySelector(".left");
+const rightHighlight = document.querySelector(".right");
+let currentTotal1 = 0;
+let currentTotal2 = 0;
+let currentPlayer = 1;
 
 roll.addEventListener("click", function () {
   const rand = Math.floor(Math.random() * 6) + 1;
-  switch (rand) {
-    case rand:
+  let currentUpdate1 = function () {
+    currentTotal1 += rand;
+    img.innerHTML = `<img src="images/${rand}-die.png" alt="" /></div>`;
+    currNum1.textContent = `${currentTotal1}`;
+  };
+  let currentUpdate2 = function () {
+    currentTotal2 += rand;
+    img.innerHTML = `<img src="images/${rand}-die.png" alt="" /></div>`;
+    currNum2.textContent = `${currentTotal2}`;
+  };
+  if (currentPlayer % 2 != 0) {
+    leftHighlight.style.backgroundColor = "rgb(255, 255, 255, 0.55)";
+    rightHighlight.style.backgroundColor = "rgb(255, 255, 255, 0.33)";
+    if (rand == 1) {
       img.innerHTML = `<img src="images/${rand}-die.png" alt="" /></div>`;
-      currNum1.textContent = `${rand}`;
-      break;
+      currNum1.textContent = `${currentTotal1}`;
+      currentPlayer++;
+    } else {
+      currentUpdate1();
+    }
+  } else {
+    rightHighlight.style.backgroundColor = "rgb(255, 255, 255, 0.55)";
+    leftHighlight.style.backgroundColor = "rgb(255, 255, 255, 0.33)";
+    if (rand == 1) {
+      img.innerHTML = `<img src="images/${rand}-die.png" alt="" /></div>`;
+      currNum1.textContent = `${currentTotal1}`;
+      currentPlayer++;
+    } else {
+      currentUpdate2();
+    }
   }
 });
