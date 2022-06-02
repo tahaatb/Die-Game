5877afceb9c573e27fb7336a1ffa2789bfccce73
Taha Attibbi <tahaatb@gmail.com>
2022-06-02 14:21:05 +0100

    Made die change according to the random number

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-'use strict';
+"use strict";
 
-const roll = document.querySelector('.roll');
-const hold = document.querySelector('.hold');
+const roll = document.querySelector(".roll");
+const hold = document.querySelector(".hold");
+const img = document.querySelector(".die");
+const currNum1 = document.querySelector(".current-number-1");
+const currNum2 = document.querySelector(".current-number-2");
+
+roll.addEventListener("click", function () {
+  const rand = Math.floor(Math.random() * 6) + 1;
+  switch (rand) {
+    case rand:
+      img.innerHTML = `<img src="images/${rand}-die.png" alt="" /></div>`;
+      currNum1.textContent = `${rand}`;
+      break;
+  }
+});
